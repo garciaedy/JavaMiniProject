3748b4d6474e290260bc7e939b7a12b2dab0156e
garciaedy <edygarcia97@gmail.com>
2023-10-10 21:47:49 -0400

    added if statement to gran number of seats

--- JavaMiniProjects/src/CinemaRoomSeating/SeatingManager.java
+++ JavaMiniProjects/src/CinemaRoomSeating/SeatingManager.java
@@ -5,10 +5,10 @@ public class SeatingManager {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Cinema");
 		System.out.println("1 2 3 4 5 6 7 8");
-		
+
 		for(int i = 0; i < 8; i++) {
 			System.out.println(i + " ");
 			for(int j = 1; j< 9; j++  ) {
@@ -18,14 +18,28 @@ public class SeatingManager {
 			System.out.println();
 
 		}
-		
-		
+
+
 		System.out.println("Enter the number of rows: ");
 		int numberOfRows = scanner.nextInt();
 		System.out.println("Enter the number of seat in each row");
 		int numberOfSeats = scanner.nextInt();
-		
-		
+
+		int totalNumberOfSeats = numberOfRows * numberOfSeats;
+		int totalIncome;
+
+		if(totalNumberOfSeats <= 60){
+			totalIncome = totalNumberOfSeats * 10;
+		}else {
+			int frontHalfOfRows = numberOfRows / 2;
+			int backHalfOfRows = numberOfRows - frontHalfOfRows;
+
+			totalIncome = frontHalfOfRows * numberOfSeats * 10
+					+ backHalfOfRows * numberOfSeats *8;
+
+
+
+		}
 	}
 
 }
