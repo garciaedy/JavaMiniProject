05d5ef7bf3e326d72b9f8f95d5a034dfc18b77de
garciaedy <edygarcia97@gmail.com>
2023-10-10 21:26:48 -0400

    updated

--- JavaMiniProjects/src/CinemaRoomSeating/SeatingManager.java
+++ JavaMiniProjects/src/CinemaRoomSeating/SeatingManager.java
@@ -3,8 +3,21 @@ package CinemaRoomSeating;
 public class SeatingManager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Cinema");
+		System.out.println("1 2 3 4 5 6 7 8");
+		
+		for(int i = 0; i < 8; i++) {
+			System.out.println(i + " ");
+			for(int j = 1; j< 9; j++  ) {
+				System.out.println("s");
 
+			}
+			System.out.println();
+
+		}
+		
+		
 	}
 
 }
